f5da80167390dcaeb82a71fcd7a8574dcf3a8cff
jubjub00 <hhabagu@gamil.com>
2021-02-23 11:00:58 +0700

    templates

--- helloTest.py
+++ helloTest.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 def Home():
-    return "<h1>Hello World</h1>"
+    return render_template("home.html")
 
-@app.route('/test')
-def test():
-    return "<h1>Hello test 123</h1>"
+@app.route('/aboutUs')
+def aboutUs():
+    return render_template("about.html")
+
+@app.route('/john')
+def john():
+    return "<h1>Hello john.</h1>"
+
+@app.route('/welcome/<name>')
+def welcome(name):
+    return "<h1>welcome "+name+"</h1>"
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='127.0.0.1', port=80)
+    app.run(debug = True)
