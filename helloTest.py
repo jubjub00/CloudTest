0ff6b6de0bddf78d3c368a1c748c3a91e0d0884d
jubjub00 <hhabagu@gmail.com>
2021-04-07 14:54:06 +0700

    test2

--- helloTest.py
+++ helloTest.py
@@ -1,21 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def Home():
     return render_template("home.html")
 
-@app.route('/aboutUs')
-def aboutUs():
-    return render_template("about.html")
+# @app.route('/aboutUs')
+# def aboutUs():
+#     return render_template("about.html")
 
-@app.route('/john')
-def john():
-    return "<h1>Hello john.</h1>"
+# @app.route('/john')
+# def john():
+#     return "<h1>Hello john.</h1>"
 
-@app.route('/welcome/<name>')
-def welcome(name):
-    return "<h1>welcome "+name+"</h1>"
+# @app.route('/welcome/<name>')
+# def welcome(name):
+#     return "<h1>welcome "+name+"</h1>"
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(host='0.0.0.0',port = 80)
